2d362ac1bd2973513992b8c387666d1acaa217fe
Nicky Liu <nickyliu91@gmail.com>
2018-09-07 04:29:44 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :employee_count
-      t.boolean :closed?
+      t.boolean :closed
       t.datetime :opening_time
       t.datetime :closing_time
       t.datetime :timestamps
