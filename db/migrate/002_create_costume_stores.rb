635a8b83a7ced823a5faa61a252b0a7153b0a22b
Nicky Liu <nickyliu91@gmail.com>
2018-09-07 04:29:24 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
-      t.integer :employees
+      t.integer :costume_inventory
+      t.integer :employee_count
       t.boolean :business?
       t.datetime :opening_time
       t.datetime :closing_time
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,6 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_time
       t.text :description
+      t.datetime :timestamps
     end
   end
 end
